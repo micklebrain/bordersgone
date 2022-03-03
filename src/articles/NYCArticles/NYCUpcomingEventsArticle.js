7667b2fc12c271d3fb90a949a94107d4e89ce0bb
Nathan Nguyen <micklebrain@gmail.com>
2022-03-03 16:26:26 -0500

    updates

--- src/articles/NYCArticles/NYCUpcomingEventsArticle.js
+++ src/articles/NYCArticles/NYCUpcomingEventsArticle.js
@@ -28,17 +28,17 @@ class NYCUpcomingEventsArticle extends React.Component {
                         <li> ongoing | Free Friday admissions | @ The Morgan library and museum </li>
                         
                         <h2> March </h2>
-                        <li> 2nd - 6th | Nitehawk Shorts Festival | Various locations </li>
-                        <li> 3rd from 2-9pm| NYC Winter Wine and Food Festival | @Webster Hall </li>
-                        <li> 4th | Rezz | @Terminal 5 </li>                    
-                        <li> 10th | Amine | @Terminal5</li>
+                        <li> 2nd - 6th | Nitehawk Shorts Festival | Various locations </li>                        
+                        <li> 4th | Rezz | @ Terminal 5 </li>
+                        <li> 10th | Amine | @ Terminal5</li>
                         <li> 13th - 14th | Tyler, The Creator | @ Madison Sqaure Garden </li>
                         <li> 17th | St Patrick's Day Parade </li>
                         <li> 18th | Reliant K | @ Irving plaza </li>
                         <li> 18th - 19th | Bad Bunny | @ Barclay Center </li>
-                        <li> 26th | JoJo | @Terminal5 </li>
-                        <li> 27th | New York Rangers vs Buffalo Sabres | @The Madison Square Garden </li>
-                        <li> 28th | New York Knicks vs Chicago Bull | @The Madison Square Garden </li>                    
+                        <li> 4th - 20th | Eataly Fest | @ Eataly Restaurant </li>
+                        <li> 26th | JoJo | @ Terminal5 </li>
+                        <li> 27th | New York Rangers vs Buffalo Sabres | @ The Madison Square Garden </li>
+                        <li> 28th | New York Knicks vs Chicago Bull | @ The Madison Square Garden </li>                    
                     </div>
 
                     <h2> April </h2>
@@ -47,10 +47,11 @@ class NYCUpcomingEventsArticle extends React.Component {
                     <h2> May </h2>
                     <li> 1st | Govenors island reopened </li>
                     <li> 1st | Japan Day Festival | @Central Park </li>
+                    <li> 22nd | Keshi | @Webster Hall </li>
                     <li> 9th | Japan Day Festival | @Central Park </li>
 
                     <h2> June </h2>
-                    <li> 7th | Seaport Cinema </li> 
+                    <li> 7th | Seaport Cinema </li>
                     <li> 9th - 20th | Tribeca Film Festival </li>
                     <li> 14th | Seaport Cinema </li>
                     <li> 19th | Queens Night market opens </li>
@@ -58,11 +59,12 @@ class NYCUpcomingEventsArticle extends React.Component {
                     <h2> July </h2>
                     <li> 31st - September 18 | Bric's Celebrates Brooklyn! Festival </li>
                     
-                    <h2> August </h2>
-                    
+                    <h2> August </h2>                    
                     <li> 1st | Pixar-themed mini-golf course </li>
                     <a href="https://www.gozerog.com/the-astronaut-experience-new-york-ny/"> 21st - 19th | Zero G Experience </a>
-                    <li> 28th | Le Poisson Rouge </li>                    
+                    <li> 12th | The Kid Laroi | Manhattan Center Hammerstein Ballroom </li>
+                    <li> 25th | Russ | Radio City Music Hall </li>
+                    <li> 28th | Le Poisson Rouge </li>
 
                     <h2> September </h2>
                     <li> 30th - September 12 | US Open </li>
