2da098b786e898dcfb9b49bdad50c4ff29bf241e
Nathan Nguyen <micklebrain@gmail.com>
2022-03-10 14:19:46 -0500

    updates

--- src/articles/NYCArticles/NYCEventsArticle.js
+++ src/articles/NYCArticles/NYCEventsArticle.js
@@ -34,14 +34,15 @@ class NYCEventsArticle extends React.Component {
 
                         <h2> March </h2>
                         <li> 10th | Amine | @ Terminal5 </li>
-                        <li> 13th - 14th | Tyler, The Creator Concert | @ Madison Sqaure Garden </li>
+                        <li> 12th | Steve Aoki | @ Madison Sqaure Garden | music </li>
+                        <li> 13th - 14th | Tyler, The Creator Concert | @ Madison Sqaure Garden | music </li>
                         <li> until March 15 | Luna X Moya: What the Pier Gave Us (2021) movie | @ Plaza at 300 Ashland Drive in Theater </li>
                         <li> 17th | St Patrick's Day Parade </li>
-                        <li> 18th | Reliant K Concert | @ Irving plaza </li>
-                        <li> 18th - 19th | Bad Bunny Concert | @ Barclay Center </li>
+                        <li> 18th | Reliant K Concert | @ Irving plaza | music </li>
+                        <li> 18th - 19th | Bad Bunny Concert | @ Barclay Center | music </li>
                         <li> 4th - 20th | Eataly Fest | @ Eataly Restaurant </li>
                         <li> March 16 - March 23 | Olalekan Jeyifous: The Frozen Neighborhoods (Fly-through) (2021) | @ Plaza at 300 Ashland Drive in Theater </li>
-                        <li> 26th | JoJo Concert | @ Terminal5 </li>
+                        <li> 26th | JoJo Concert | @ Terminal5 | music </li>
                         <li> 27th | New York Rangers vs Buffalo Sabres | @ The Madison Square Garden </li>
                         <li> 28th | New York Knicks vs Chicago Bull | @ The Madison Square Garden </li>
                         <li> March 25 - March 30 | Tanika I. Williams: (construct)Clearing (2021) and Sanctuary (2021) | @ Plaza at 300 Ashland Drive in Theater </li>
@@ -50,7 +51,7 @@ class NYCEventsArticle extends React.Component {
                     </div>
 
                     <h2> April </h2>
-                    <li> 8th | Lil Tecca | @Terminal 5 </li>
+                    <li> 8th | Lil Tecca | @Terminal 5 | music</li>
                     <li> April 6 - April 13  | Aisha Amin: Choir (2020) and Friday (2019) | @ Plaza at 300 Ashland Drive in Theater </li>
 
                     <h2> May </h2>
--- src/articles/ParisArticles/ParisThingsToDoArticle.js
+++ src/articles/ParisArticles/ParisThingsToDoArticle.js
@@ -14,25 +14,27 @@ class ParisThingsToDoArticle extends React.Component {
         return (
             <div>
                 <h1> Things to do in Paris </h1>
-                <ul>
-                    <li> Arc de Triomphe </li> 
-                    <li> Boat tour </li>
+                <ul>                    
                     <li> Catacombs </li>
                     <li> Cheese tasting </li>
-                    <li> Eiffel Tower </li>
-                    <li> Louvre </li>
-                    <li> Musee d’Orsay </li>                    
-                    <li> Palais Garnier </li>
-                    <li> Pantheon </li>
-                    <li> Seine River boat / cruise tour </li>
+                    <li> Moulin Rogue show </li>                     
                     <li> Wine tasting </li>
                 </ul>
+
+                <h2> Monuments </h2>
+                <li> Arc de Triomphe </li>
+                <li> Eiffel Tower </li>                
+                <li> Musee d’Orsay </li>
+                <li> Palais Garnier </li>
+                <li> Pantheon </li>
+
+                <h2> Musuems </h2>
+                <li> Louvre </li>
+
                 <h2> Seine River boat / cruise tour </h2>                
                 <li> Bateaux Mouches seina cruise </li>
                 <li> Bateaux Parisiens seina cruise </li>
                 <li> BatoBus hop on hop off cruise </li>
-
-                <h2> Musuems </h2>
 
                 <iframe src="https://www.google.com/maps/d/embed?mid=1MGy4jfdB2-kJmiaFrmzqt44T3fnTcu1n&ehbc=2E312F" width="640" height="480"></iframe>
             </div>)
