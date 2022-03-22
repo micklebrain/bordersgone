96cd7663c278688e7480e10fb5bf990ca2e74dbe
Nathan Nguyen <micklebrain@gmail.com>
2022-03-22 13:46:08 -0400

    updates

--- src/articles/NYCArticles/NYCEventsArticle.js
+++ src/articles/NYCArticles/NYCEventsArticle.js
@@ -20,6 +20,7 @@ class NYCEventsArticle extends React.Component {
                         <li> until April 9 | New York on Paper Exhibit | @ West Chelsea Contemporary </li>                        
                         <li> until May 1st | Orchid show | @ New York Botanical Garden </li>
                         <li> every Saturday until May 28 | MoonRise: A Nouveau Circus Immersive Performance | 1329 Willoughby Ave, Brooklyn 11237</li>
+                        <li> until May 15th | Deepfake: Unstable Evidence on Screen | @ Changing Exhibitions Gallery </li>
                         <li> until May 30th | The Butterfly Conservatory exhibit | @ American Museum of Natural History </li>
                         <li> until June 30th | The Fans Strke Back Star Wars Exhibit | @ 526 6th Avenue </li>
                         <li> until August 14th | Sharks exhibit | @ American Museum of Natural History </li>
@@ -29,18 +30,18 @@ class NYCEventsArticle extends React.Component {
                         <li> ongoing | Free Friday admissions from 6-10pm | @ The Rubin Museum of Art </li>
                         <li> ongoing | Free Friday admissions | @ The Morgan library and museum </li>
 
-                        <h2> March </h2>                   
+                        <h2> March </h2>                                           
+                        <li> 16th - 23rd | Olalekan Jeyifous: The Frozen Neighborhoods (Fly-through) (2021) | @ Plaza at 300 Ashland Drive in Theater </li>
                         <li> 24th | Mitski | @ Radio City Hall | music </li>
-                        <li> March 16 - March 23 | Olalekan Jeyifous: The Frozen Neighborhoods (Fly-through) (2021) | @ Plaza at 300 Ashland Drive in Theater </li>
                         <li> 26th | JoJo Concert | @ Terminal5 | music </li>
                         <li> 27th | New York Rangers vs Buffalo Sabres | @ The Madison Square Garden </li>
                         <li> 28th | New York Knicks vs Chicago Bull | @ The Madison Square Garden </li>
-                        <li> March 25 - March 30 | Tanika I. Williams: (construct)Clearing (2021) and Sanctuary (2021) | @ Plaza at 300 Ashland Drive in Theater </li>
-                        <li> until 31st | Tomas Saraceno Patircular Matter(s) | @ The Shed </li>
-                        <li> March 31 - April 5  | Series of shorts by Ezra Wube: Flatbushtopia (2017), Bridge Street (2015), At the Same Moment (2013), Words of Wisdom (2016) | @ Plaza at 300 Ashland Drive in Theater </li>
+                        <li> 25th - 30th | Tanika I. Williams: (construct)Clearing (2021) and Sanctuary (2021) | @ Plaza at 300 Ashland Drive in Theater </li>
+                        <li> until 31st | Tomas Saraceno Patircular Matter(s) | @ The Shed </li>                        
                     </div>
 
                     <h2> April </h2>
+                    <li> 1st - 5th  | Series of shorts by Ezra Wube: Flatbushtopia (2017), Bridge Street (2015), At the Same Moment (2013), Words of Wisdom (2016) | @ Plaza at 300 Ashland Drive in Theater </li>
                     <li> 2nd | New York Knicks vs Knicks | @ The Madison Square Garden </li>
                     <li> 7th | International Pillow Fight Day | @ Parks around the city </li>
                     <li> 8th - 9th | Lil Tecca | @ Terminal 5 | music</li>
@@ -53,7 +54,11 @@ class NYCEventsArticle extends React.Component {
                     <h2> May </h2>
                     <li> 1st | Govenors Island reopened </li>
                     <li> 1st | Japan Day Festival | @ Central Park </li>
+                    <li> 5th | ALESSO | @ Advant Gardner </li>
+                    <li> 19th | DAVID GUETTA & MORTEN PRESENT FUTURE RAVE | @ Advant Gardner | music </li>
+                    <li> 20th 8 PM | DAVID GUETTA | Avant Gardner</li> 
                     <li> 22nd | Keshi | @ Webster Hall </li>
+                    <li> 25th | Russ | @ Radio City Music Hall </li>
                     <li> 9th | Japan Day Festival | @ Central Park </li>
 
                     <h2> June </h2>
