183456fe6d9d493f9ba61802d270616b7def8ed9
Nathan Nguyen <micklebrain@gmail.com>
2022-03-23 21:52:00 -0400

    updates

--- src/articles/NYCArticles/NYCEventsArticle.js
+++ src/articles/NYCArticles/NYCEventsArticle.js
@@ -303,6 +303,26 @@ class NYCEventsArticle extends React.Component {
                                 <div class='event'>
                                     <div class='event-date'>
                                         <div>
+                                            <div class="event-date-day">26</div>
+                                            <div class="event-date-month">MAR</div>
+                                        </div>
+                                    </div>
+                                    <div class='event-main'>
+                                        <div class="event-main-title" aria-level="3" role="heading"> Elephante </div>
+                                        <div class="event-details">
+                                            <div class="event-details-date"> All Day </div>
+                                            <div class="event-details-location">Webster Hall</div>
+                                            <div class="event-details-location-2">New York NY</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class='event-li'>
+                            <div class='event-outer'>
+                                <div class='event'>
+                                    <div class='event-date'>
+                                        <div>
                                             <div class="event-date-day"> 26 </div>
                                             <div class="event-date-month">MAR</div>
                                         </div>
