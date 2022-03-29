9c25f78d1158c97f188fbe4e4c344bc2803affb7
Nathan Nguyen <micklebrain@gmail.com>
2022-03-29 09:48:52 -0400

    updates

--- src/articles/NYCArticles/NYCEventsArticle.js
+++ src/articles/NYCArticles/NYCEventsArticle.js
@@ -65,7 +65,7 @@ class NYCEventsArticle extends React.Component {
                                 <div class='event'>
                                     <div class='event-date'>
                                         <div>
-                                            <div class="event-date-day"> Until 17 </div>
+                                            <div class="event-date-day">until 17</div>
                                             <div class="event-date-month">APR</div>
                                         </div>
                                     </div>
@@ -114,6 +114,26 @@ class NYCEventsArticle extends React.Component {
                                         <div class="event-details">
                                             <div class="event-details-date"> All Day </div>
                                             <div class="event-details-location"> New York Botanical Garden </div>
+                                            <div class="event-details-location-2">New York, NY </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class='event-li'>
+                            <div class='event-outer'>
+                                <div class='event'>
+                                    <div class='event-date'>
+                                        <div>
+                                            <div class="event-date-day">8-4</div>
+                                            <div class="event-date-month">APR-May</div>
+                                        </div>
+                                    </div>
+                                    <div class='event-main'>
+                                        <div class="event-main-title" aria-level="3" role="heading">The Poem Mural</div>
+                                        <div class="event-details">
+                                            <div class="event-details-date">All Day</div>
+                                            <div class="event-details-location">The Poem</div>
                                             <div class="event-details-location-2">New York, NY </div>
                                         </div>
                                     </div>
