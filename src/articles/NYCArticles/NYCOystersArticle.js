b594171d64771757a7dfe372a3454ac16fef65f6
mehst <80065596+mehst@users.noreply.github.com>
2022-04-05 16:08:21 -0400

    updates

--- src/articles/NYCArticles/NYCOystersArticle.js
+++ src/articles/NYCArticles/NYCOystersArticle.js
@@ -12,12 +12,12 @@ class NYCArticle extends React.Component {
     render() {            
         return (            
             <div>
-                <h1> NYC $1 Dollar Oysters </h1>
-                <li> Bar Belly daily from 5-7pm </li>
+                <h1> NYC $1 Oysters guide </h1>
+                <li> Bar Belly daily from 5-7pm ($1) </li>
                 <li> Black Crescent daily from 5-7pm ($2 each) </li>
                 <li> Flex Mussels daily ($1.5 each) </li>
                 <li> Good Night Sonny Tuesday - Fridays from 4-6pm ($15 for a dozen) </li>
-                <li> Maracuja daily from 4-7pm </li>
+                <li> Maracuja daily from 4-7pm ($1) </li>
                 <li> The Wayland weekdays from 4-6pm ($18 for a dozen) </li>
         </div>)
     }
