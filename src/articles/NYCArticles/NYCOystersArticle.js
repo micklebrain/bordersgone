e684b97301ffaa988ef6ab0e2ef5d88027312f4e
mehst <80065596+mehst@users.noreply.github.com>
2022-04-05 16:05:25 -0400

    updates

--- src/articles/NYCArticles/NYCOystersArticle.js
+++ src/articles/NYCArticles/NYCOystersArticle.js
@@ -13,15 +13,12 @@ class NYCArticle extends React.Component {
         return (            
             <div>
                 <h1> NYC $1 Dollar Oysters </h1>
-                <li> Bar Belly Fridays from 5-7pm </li>                
-                <li> Black Crescent from 6-8pm </li>
-                <li> Brooklyn Crab Tuesday and Wednesday </li>                
-                <li> Flex Mussels from 5:30-11pm </li>
-                <li> Good Night Sonny Tuesday - Fridays from 4-6pm (One dozen for $15) </li>
-                <li> Lighthouse from 4-7pm </li>
-                <li> Maracuja from 4-7pm </li>
-                <li> Pinkerton Wine Bar all the time </li>
-                <li> The Wayland Monday - Friday from 4-6pm </li>
+                <li> Bar Belly daily from 5-7pm </li>
+                <li> Black Crescent daily from 5-7pm ($2 each) </li>
+                <li> Flex Mussels daily ($1.5 each) </li>
+                <li> Good Night Sonny Tuesday - Fridays from 4-6pm ($15 for a dozen) </li>
+                <li> Maracuja daily from 4-7pm </li>
+                <li> The Wayland weekdays from 4-6pm ($18 for a dozen) </li>
         </div>)
     }
 }
