df8b0c7ed184773583440db37650e4498cda40d8
Nathan Nguyen <micklebrain@gmail.com>
2022-04-10 14:12:23 -0400

    updates

--- src/articles/NYCArticles/NYCSpeakeasiesArticle.js
+++ src/articles/NYCArticles/NYCSpeakeasiesArticle.js
@@ -14,6 +14,11 @@ class NYCSpeakeasiesArticle extends React.Component {
             <div>
                 <h1> NYC speakeasy Guide </h1>
                 <li> Angel Share </li>
+                <li> Attaboy </li>
+                <li> Little Branch </li>
+                <li> Nothing Really Matters </li>
+                <li> PDT </li>
+                <li> Thyme Bar </li>
         </div>)
     }
 }
