cde85363e20843245c6cd2449a716650106d78f4
mehst <80065596+mehst@users.noreply.github.com>
2022-05-10 16:25:13 -0400

    updates

--- src/customItinerary.js
+++ src/customItinerary.js
@@ -23,6 +23,19 @@ async function Payment(payment, setPayment, paymentIntent) {
     setPayment([...payment, paymentStatus]);
 }
 
+function sendEmail() {
+    var requestOptions = {
+        method: 'POST',
+        redirect: 'follow'
+      };
+      
+      fetch("https://lostmindsbackend.vercel.app/sendemail", requestOptions)
+    //   fetch("http://localhost:3000/sendemail", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
 export default function CustomItinerary() {
     const [payment, setPayment] = useState([]);
 
@@ -30,50 +43,52 @@ export default function CustomItinerary() {
     var payment_intent = queryParams.get("payment_intent");
     var payment_intent_client_secret = queryParams.get("payment_intent_client_secret");
 
-    if (payment_intent == null) {
-        return (<div>
-            <h1> Payment must be made </h1>
-        </div>)
-    }
+    sendEmail();
 
-    if (payment == null || payment != 'succeeded') {
-        Payment(payment, setPayment, payment_intent);
-        return (<div>
-            <h1> Payment must be made </h1>
-        </div>)
-    } else {
+    // if (payment_intent == null) {
+    //     return (<div>
+    //         <h1> Payment must be made </h1>
+    //     </div>)
+    // }
+
+    // if (payment == null || payment != 'succeeded') {
+    //     Payment(payment, setPayment, payment_intent);
+    //     return (<div>
+    //         <h1> Payment must be made </h1>
+    //     </div>)
+    // } else {
         return (
             <div >
                 <h1> Day trip in NYC </h1>
                 <ul>
                     <h1>Morning</h1>
                     <h2>Breakfast</h2>
-                    <li>7:00 am - Le Pain Quotidien</li>
-                    <li>9:00 am - Walk highline</li>
-                    <li>11:00 am - Chelsea Market</li>
+                    <li>7am - Le Pain Quotidien</li>
+                    <li>9am - Walk highline</li>
+                    <li>11am - Chelsea Market</li>
 
                     <h1>Afternoon </h1>
                     <h2>Lunch </h2>
-                    <li>12:00 pm - Obao</li>
-                    <li>2:00 pm - Vessel</li>
-                    <li>4:00 pm - Empire State Building</li>
+                    <li>12pm - Obao</li>
+                    <li>2pm - Vessel</li>
+                    <li>4pm - Empire State Building</li>
 
-                    <h1>Nightlife</h1>
                     <h2>Dinner </h2>
-                    <li>6:00 pm - Barn Joo NoMad</li>
+                    <li>6pm - Barn Joo NoMad</li>
 
                     <h2>Dessert </h2>
-                    <li>8:00 pm - Venchi</li>
+                    <li>8pm - Venchi</li>
 
+                    <h1>Nightlife</h1>
                     <h2>Play </h2>
-                    <li>9:00 pm Dave and Buster</li>
+                    <li>9pm Dave and Buster</li>
 
                     <h2>Drinks - Speakeasy</h2>
-                    <li>10:00 pm Dear Irving Gramercy</li>
+                    <li>12am Dear Irving Gramercy</li>
                     <h2>Dance</h2>
-                    <li>12:00 pm Mission nightclub</li>
+                    <li>2am Mission nightclub</li>
                 </ul>
             </div>
         );
-    }
+    // }
 }
