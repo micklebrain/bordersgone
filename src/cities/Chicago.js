47c840b8016d4f5c9a7eaeb4d8a27889f75df1c8
Nathan Nguyen <micklebrain@gmail.com>
2022-07-05 21:13:01 -0400

    updates

--- src/cities/Chicago.js
+++ src/cities/Chicago.js
@@ -11,11 +11,7 @@ class Chicago extends React.Component {
     render() {
         return (
             <div class='container'>
-                <Sidebar
-                    // events={this.state.events}
-                    // onEventsChange={this.handleEventsChange}>
-                    >
-                </Sidebar>
+                <Sidebar />
                 <h1> Chicago travel guide </h1>
                 <Link to="/ChicagoArticle" class="articlePreview"> Top 5 must do things in Chicago </Link>
                 <Link to="/ChicagoArticle2" class="articlePreview"> Top 5 restaurants in Chicago </Link>
