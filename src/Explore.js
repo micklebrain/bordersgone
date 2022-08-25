3f810587db96a2e22b0503a42796f6a7aa86e7f3
Nathan Nguyen <micklebrain@gmail.com>
2022-08-25 00:09:18 -0400

    updates

--- src/Explore.js
+++ src/Explore.js
@@ -31,24 +31,24 @@ function Explore() {
     <div className="App">
       <Sidebar/>
       <div class="column-main">
-        <Link to="/atlanta" class="articlePreview"> <img class="articleImage" src={atlanta} alt="Atlanta" /> </Link> <div> <div class="articleTitle"> Atlanta </div> </div>
-        <Link to="/boston" class="articlePreview"> <img class="articleImage" src={boston} alt="Boston" /> </Link> <div> <div class="articleTitle"> Boston </div> </div>
-        <Link to="/denver" class="articlePreview"> <img class="articleImage" src={denver} alt="Denver" /> </Link> <div> <div class="articleTitle"> Denver </div> </div>
-        <Link to="/newyorkcity" class="articlePreview"> <img class="articleImage" src={newyorkcity} alt="New York City" /> </Link> <div> <div class="articleTitle"> New York City </div> </div>
-        <Link to="/philadelphia" class="articlePreview"> <img class="articleImage" src={philadelphia} alt="Philadelphia" /> </Link> <div> <div class="articleTitle"> Philadelphia </div> </div>
-        <Link to="/seattle" class="articlePreview"> <img class="articleImage" src={seattle} alt="Seattle" /> </Link> <div> <div class="articleTitle"> Seattle </div> </div>
-        <Link to="/washingtonDC" class="articlePreview"> <img class="articleImage" src={washingtonDC} alt="Washington DC" /> </Link> <div> <div class="articleTitle"> Washington DC </div> </div>
+        <Link to="/atlanta" class="articlePreview"> <img class="articleImage" src={atlanta} alt="Atlanta" /> </Link> <div> <div class="articleTitle">Atlanta</div> </div>
+        <Link to="/boston" class="articlePreview"> <img class="articleImage" src={boston} alt="Boston" /> </Link> <div> <div class="articleTitle">Boston</div> </div>
+        <Link to="/denver" class="articlePreview"> <img class="articleImage" src={denver} alt="Denver" /> </Link> <div> <div class="articleTitle">Denver</div> </div>
+        <Link to="/newyorkcity" class="articlePreview"> <img class="articleImage" src={newyorkcity} alt="New York City" /> </Link> <div> <div class="articleTitle">New York City</div> </div>
+        <Link to="/philadelphia" class="articlePreview"> <img class="articleImage" src={philadelphia} alt="Philadelphia" /> </Link> <div> <div class="articleTitle">Philadelphia</div> </div>
+        <Link to="/seattle" class="articlePreview"> <img class="articleImage" src={seattle} alt="Seattle" /> </Link> <div> <div class="articleTitle">Seattle</div> </div>
+        <Link to="/washingtonDC" class="articlePreview"> <img class="articleImage" src={washingtonDC} alt="Washington DC" /> </Link> <div> <div class="articleTitle">Washington DC</div> </div>
         <Link to="/singapore" class="articlePreview"> <img class="articleImage" src={singapore} alt="Singapore" /> </Link> <div> <div class="articleTitle">Singapore</div> </div>
-        <Link to="/Bangkok" class="articlePreview"> <img class="articleImage" src={bangkok} alt="Bangkok" /> </Link> <div> <div class="articleTitle">Thailand</div> </div>
+        <Link to="/Bangkok" class="articlePreview"> <img class="articleImage" src={bangkok} alt="Bangkok" /> </Link> <div> <div class="articleTitle">Bangkok</div> </div>
       </div>
       <div class="column-main">
-        <Link to="/bogota" class="articlePreview"> <img class="articleImage" src={bogota} alt="Chicago" /> </Link> <div> <div class="articleTitle"> Bogota </div> </div>
-        <Link to="/chicago" class="articlePreview"> <img class="articleImage" src={chicago} alt="Chicago" /> </Link> <div> <div class="articleTitle"> Chicago </div> </div>
-        <Link to="/lasvegas" class="articlePreview"> <img class="articleImage" src={lasvegas} alt="Las Vegas" /> </Link> <div> <div class="articleTitle"> Las Vegas </div> </div>
-        <Link to="/paris" class="articlePreview"> <img class="articleImage" src={paris} alt="New York City" /> </Link> <div> <div class="articleTitle"> Paris </div> </div>
-        <Link to="/providence" class="articlePreview"> <img class="articleImage" src={providence} alt="Providence" /> </Link> <div> <div class="articleTitle"> Providence </div> </div>
-        <Link to="/sanfrancisco" class="articlePreview"> <img class="articleImage" src={sanfrancisco} alt="San Francisco" /> </Link> <div> <div class="articleTitle"> San Francisco </div> </div>
-        <Link to="/toronto" class="articlePreview"> <img class="articleImage" src={toronto} alt="Toronto" /> </Link> <div> <div class="articleTitle"> Toronto </div> </div>
+        <Link to="/bogota" class="articlePreview"> <img class="articleImage" src={bogota} alt="Chicago" /> </Link> <div> <div class="articleTitle">Bogota</div> </div>
+        <Link to="/chicago" class="articlePreview"> <img class="articleImage" src={chicago} alt="Chicago" /> </Link> <div> <div class="articleTitle">Chicago</div> </div>
+        <Link to="/lasvegas" class="articlePreview"> <img class="articleImage" src={lasvegas} alt="Las Vegas" /> </Link> <div> <div class="articleTitle">Las Vegas</div> </div>
+        <Link to="/paris" class="articlePreview"> <img class="articleImage" src={paris} alt="New York City" /> </Link> <div> <div class="articleTitle">Paris</div> </div>
+        <Link to="/providence" class="articlePreview"> <img class="articleImage" src={providence} alt="Providence" /> </Link> <div> <div class="articleTitle">Providence</div> </div>
+        <Link to="/sanfrancisco" class="articlePreview"> <img class="articleImage" src={sanfrancisco} alt="San Francisco" /> </Link> <div> <div class="articleTitle">San Francisco</div> </div>
+        <Link to="/toronto" class="articlePreview"> <img class="articleImage" src={toronto} alt="Toronto" /> </Link> <div> <div class="articleTitle">Toronto</div> </div>
         <Link to="/kualalumpur" class="articlePreview"> <img class="articleImage" src={kualalumpur} alt="Kuala Lumpur" /> </Link> <div> <div class="articleTitle">Kuala Lumpur</div> </div>
         <Link to="/tokyo" class="articlePreview"> <img class="articleImage" src={tokyo} alt="Tokyo" /> </Link> <div> <div class="articleTitle">Tokyo</div> </div>
         <Link to="/seoul" class="articlePreview"> <img class="articleImage" src={tokyo} alt="Seoul" /> </Link> <div> <div class="articleTitle">Seoul</div> </div>
--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Login from './Login'
 import Sidebar from './sidebar'
+import {useState} from 'react';
 
 function Bars(props) {
   var today = new Date();
@@ -33,7 +34,9 @@ function NightTime(props) {
     return <div>
       <h1>Clubs</h1>
       <li>Mission</li>
+      <Button>Buy table $100</Button>
       <li>Slate</li>
+      <Button>Buy table $100</Button>
     </div>
   } else {
     return <div></div>
@@ -46,7 +49,9 @@ function DinnerSpots(props) {
     return <div>
       <h1>Dinner</h1>
       <li>Anytime Kitchen</li>
+      <Button>Reserve table with bottle - $20</Button>
       <li>Rib No. 7</li>
+      <Button>Reserve table with bottle - $20</Button>
     </div>
   } else {
     return <div></div>
@@ -55,47 +60,69 @@ function DinnerSpots(props) {
 
 function Pools() {
   // Check if summertime
-  return <div>
-    <h1>Pools</h1>
-    <li>Swimming pool</li>
-  </div>
+  var today = new Date();
+  if (today.getHours() < 19) {
+    return <div>
+      <h1>Pools</h1>
+      <li>Swimming pool</li>
+    </div>
+  } else {
+    return <div></div>
+  }
 }
 
 function Parks() {
-  return <div>
-    <h1>Parks</h1>
-    <li>Bryant Park</li>
-    <li>Madison Square Park</li>
-  </div>
+  var today = new Date();
+  if (today.getHours() < 19) {
+    return <div>
+      <h1>Parks</h1>
+      <li>Bryant Park</li>
+      <li>Madison Square Park</li>
+    </div>
+  } else {
+    return <div></div>
+  }
 }
 
 function Restuarants() {
   return <div>
     <h1>Restuarants</h1>
     <li>Arno</li>
-    <Button>Book Reservation</Button>
+    <Button>Reserve table with bottle - $20</Button>
     <li>L'Amico</li>
-    <Button>Book Reservation</Button>
+    <Button>Reserve table with bottle - $20</Button>
     <li>Trademark Bar + Kitchen</li>
-    <Button>Book Reservation</Button>
+    <Button>Reserve table with bottle - $20</Button>
   </div>
 }
+
+// function orderId() {
+//   console.log("Order ID: abc123");
+//   setButtonText('Payment request sent');
+// }
 
 function Movies() {
 
   var today = new Date();
+  const [buttonText, setButtonText] = useState('Buy ticket - $15');
 
   const details = {
     name: "AMC 34th Street 14 - Nope",
     date: today.toLocaleDateString(),
-    description: "Caretakers at a California horse ranch encounter a mysterious force that affects human and animal behaviour. Tickets will be sent to email on account"
+    description: "Caretakers at a California horse ranch encounter a mysterious force that affects human and animal behaviour. Best seats in middle of theater will be chosen automatically Tickets will be sent to email on account"
   }
+
+  const linkStyle = {
+    margin: "1rem",
+    textDecoration: "none",
+    color: 'blue',
+    textAlign: 'center'
+  };
 
   return <div>
     <h1>Theaters</h1>
-
-    <Link to="/eventDetails" state={details}>AMC 34th Street 14 - Nope</Link>
-    <Button>Buy Ticket - $15</Button>
+    <Link to="/eventDetails" state={details} style={linkStyle}>Nope - AMC 34th Street 14 @6pm</Link>
+    <Button onClick={() => {setButtonText('Payment request sent for Order ID: abc123');}} id='ticketBuy'>{buttonText}</Button>
     <li>AMC Empire 25 - Nope</li>
     <Button>Buy Ticket - $15</Button>
     <li>Rooftop Cinema Club Midtown - Nope</li>
@@ -120,13 +147,17 @@ function Casinos() {
 
 function Home() {
 
-  var today = new Date();
+  var today = new Date();  
 
   return (
     <div className="App">
       <Sidebar />
-      <h1>New York City - Manhattan</h1>
+
+      <h1>Manhattan - New York City</h1>
       <h1> {today.getHours()}:{today.getMinutes()} </h1>
+
+      <h1>Venmo: micklebrain</h1>
+      <h1>Cashapp: micklebrain</h1>
 
       <Bars />
       <LunchTime />
