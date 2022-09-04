82676b1790ed62ad55e809814ed02cf9784eb9a9
Nathan Nguyen <micklebrain@gmail.com>
2022-09-04 15:06:19 -0400

    updates

--- src/ArticlePreview.js
+++ src/ArticlePreview.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useSearchParams } from 'react';
 import { Link } from "react-router-dom";
-import nyc from './images/nyc.jpg'
+import ramen from './images/ramen.jpeg';
 
 const ArticlePreview = (props) => {
     return (
         <div>
-            <Link to="/newyorkcity/NYCEventsArticle" class="articlePreview"> <img class="articleImage" src={nyc} alt="Events" /> </Link> <div> <div class="articleTitle"> Events </div> <div>Jan 27, 2022</div> </div>
+            <Link to="/newyorkcity/NYCRamenArticle" class="articlePreview"> <img class="articleImage" src={ramen} alt="NYC Ramen guide" /> </Link> <div> <div class="articleTitle"> NYC Ramen Guide </div> <div> Jan 27, 2022 </div> </div>
         </div>
     );
 };
